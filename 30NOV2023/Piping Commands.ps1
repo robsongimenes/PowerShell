9de1b73ec9d1bdcee94e5f03fb120fc3032913b3
robsongimenes <151543367+robsongimenes@users.noreply.github.com>
2023-12-01 14:14:24 +0000

    Update Piping Commands.ps1

--- 30NOV2023/Piping Commands.ps1
+++ 30NOV2023/Piping Commands.ps1
@@ -1,1 +1,5 @@
-﻿Dir | Format-Table | Out-Host
+#DIR:  Get the list of items in the current directory
+#Format-Table: Format the output of the directory listing as a table
+#Out-Host: Send the formatted output to the host for display
+ 
+ Dir | Format-Table | Out-Host
